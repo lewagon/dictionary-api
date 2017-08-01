df87dd3c2b17064b9d7bf1ca064fd3301207ba7a
Sébastien Saunier <seb@saunier.me>
2017-08-01 23:53:18 +0200

    Autocomplete API

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,6 +1,11 @@
 class WordsController < ActionController::API
   def home
-    render json: { message: 'welcome', endpoints: ['https://wagon-dictionary.herokuapp.com/{word}'] }
+    render json: { message: 'welcome',
+      endpoints: [
+        'https://wagon-dictionary.herokuapp.com/:word',
+        'https://wagon-dictionary.herokuapp.com/autocomplete/:stem'
+      ]
+    }
   end
 
   def query
@@ -9,9 +14,20 @@ class WordsController < ActionController::API
       response = { found: true, word: word, length: word.length }
       render json: response, status: 200
     else
-      response = { found: false, error: 'word not found' }
+      response = { found: false, word: params[:word], error: 'word not found' }
       render json: response, status: 404
     end
+  end
+
+  MAX_AUTOCOMPLETE_RESULTS = 15
+
+  def autocomplete
+    matching_words = words.select { |w| w.starts_with?(params[:stem]) }
+    render json: {
+      words: matching_words[0, MAX_AUTOCOMPLETE_RESULTS],
+      count: matching_words.size,
+      truncated_result: matching_words.size > MAX_AUTOCOMPLETE_RESULTS
+    }
   end
 
   def redirect
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'words#home'
   get '/:word', to: 'words#query', as: 'query'
+  get '/autocomplete/:stem', to: 'words#autocomplete', as: 'autocomplete'
 
   get '*unmatched_route', to: 'words#redirect'
 end
