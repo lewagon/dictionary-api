12357af5fc40968766cfd833f87f84e7519d9a93
Sébastien Saunier <seb@saunier.me>
2017-08-02 12:22:38 +0200

    Open Bar - Rack cors

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 gem 'pg'
 gem 'puma'
 gem 'rails'
+gem 'rack-cors', :require => 'rack/cors'
 
 group :development, :test do
   gem 'listen'
--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,12 @@ module WagonDictionary
   class Application < Rails::Application
     config.load_defaults 5.1
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
